7011d024607e7601b31fa019d22e7f102af43d66
Sergey Burmisov <skypath@gmail.com>
2016-01-26 14:23:56 +0300

    fix lint: console

--- src/cam/cam.js
+++ src/cam/cam.js
@@ -3,11 +3,11 @@ var sensors = require('./sensors');
 var files = require('./files');
 var socket = require('socket.io-client').connect(options.serverAddress);
 
-socket.on('connect', function () {
+socket.on('connect', function cb() {
   console.log('connected to server ', options.serverAddress);
 });
 
-socket.on('disconnect', function () {
+socket.on('disconnect', function cb() {
   console.log('disconnected from server');
 });
 
